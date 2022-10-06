00a60e0dc1e9a59738f0ba11da106b7311660702
Gaspar Segura <gaspar_segura_97@hotmail.com>
2022-10-06 19:58:26 -0300

    Clase para Red de Petri

---
+++ TP_Final-Programacion_Concurrente/src/PetriNet.java
@@ -0,0 +1,18 @@
+public class PetriNet {
+
+    private int [][] incMatrix;
+    private int [] sensTransitions;
+    private int [] initialMarking;
+    private int [] currentMarking;
+
+    public PetriNet(int [][] incMatrix, int [] initialMarking){
+        this.incMatrix = incMatrix;
+        this.initialMarking = initialMarking;
+    }
+
+    // fireTransition
+    // getCurrentMarking
+    // getSensTransitions
+    // updateMarking
+    // checkCo
+}
