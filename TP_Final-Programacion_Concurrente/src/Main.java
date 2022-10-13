21ef91cc4e915bc5faeefc5d2dae428b0ea88db3
Gaspar Segura <gaspar_segura_97@hotmail.com>
2022-10-13 20:06:06 -0300

    Funcion para calcular transiciones sensibilizadas y definicion de Constantes

--- TP_Final-Programacion_Concurrente/src/Main.java
+++ TP_Final-Programacion_Concurrente/src/Main.java
@@ -1,2 +1,11 @@
+import utils.Constants;
+import utils.Matrix;
+
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args){
+        System.out.println(Arrays.toString(Matrix.transpose(Constants.INCIDENCE_MATRIX)[0]));
+    }
 }
+
--- TP_Final-Programacion_Concurrente/src/PetriNet.java
+++ TP_Final-Programacion_Concurrente/src/PetriNet.java
@@ -1,19 +1,42 @@
+import utils.Constants;
 import utils.Matrix;
+import java.util.Arrays;
 
 public class PetriNet {
 
     private int [][] incMatrix;
     private int [][] sensTransitions;
-    private int [][] initialMarking;
     private int [][] currentMarking;
 
     public PetriNet(int [][] incMatrix, int [][] initialMarking){
         this.incMatrix = incMatrix;
-        this.initialMarking = initialMarking;
+        this.currentMarking = initialMarking;
     }
 
     // fireTransition
-    public int fireTransition(int[][] fireSequence){
+
+    // getCurrentMarking
+    public int[][] getCurrentMarking(){
+        return currentMarking;
+    }
+    // getSensTransitions
+    public int[][] getSensTransitions(){
+        int[][] sensTransitions = new int[0][Constants.TRANSITIONS_COUNT];
+
+        for (int i = 0; i < Constants.TRANSITIONS_COUNT; i++){
+            for (int j = 0; j < Constants.PLACES_COUNT; j++){
+                if (incMatrix[j][i] == 1 && incMatrix[j][i] != currentMarking[0][j]){
+                    sensTransitions[0][j] = 0;
+                    continue;
+                }
+                sensTransitions[0][j] = 1;
+            }
+        }
+        return sensTransitions;
+    }
+    // updateMarking
+
+    public int updateMarking(int[][] fireSequence){
 
         try {
             currentMarking = Matrix.add(currentMarking, Matrix.multiply(incMatrix ,fireSequence));    // Fundamental equation mk = mi + W*s
@@ -24,8 +47,5 @@ public class PetriNet {
 
         return 0;
     }
-    // getCurrentMarking
-    // getSensTransitions
-    // updateMarking
-    // checkCo
+
 }
