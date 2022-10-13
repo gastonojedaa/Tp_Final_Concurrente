c36ea24c59418052c2b1f97196cbebaf8a0428f9
Gaspar Segura <gaspar_segura_97@hotmail.com>
2022-10-13 17:30:09 -0300

    Funcion para calcular matriz transpuesta y funcion de actualizacion de marcado

--- TP_Final-Programacion_Concurrente/src/PetriNet.java
+++ TP_Final-Programacion_Concurrente/src/PetriNet.java
@@ -1,16 +1,29 @@
+import utils.Matrix;
+
 public class PetriNet {
 
     private int [][] incMatrix;
-    private int [] sensTransitions;
-    private int [] initialMarking;
-    private int [] currentMarking;
+    private int [][] sensTransitions;
+    private int [][] initialMarking;
+    private int [][] currentMarking;
 
-    public PetriNet(int [][] incMatrix, int [] initialMarking){
+    public PetriNet(int [][] incMatrix, int [][] initialMarking){
         this.incMatrix = incMatrix;
         this.initialMarking = initialMarking;
     }
 
     // fireTransition
+    public int fireTransition(int[][] fireSequence){
+
+        try {
+            currentMarking = Matrix.add(currentMarking, Matrix.multiply(incMatrix ,fireSequence));    // Fundamental equation mk = mi + W*s
+        } catch(Exception e) {
+            // TODO Log exception when Logger its implemented
+            return -1;
+        }
+
+        return 0;
+    }
     // getCurrentMarking
     // getSensTransitions
     // updateMarking
